2dc80346169d74f80f06ab1ded305068251f8204
vladyslavsaienko <vladyslav.saienko@onicore.io>
2024-10-18 18:04:34 +0300

    init project on vercel

--- api/index.js
+++ api/index.js
@@ -1,5 +1,32 @@
+const axios = require("axios");
+const dotenv = require("dotenv");
 const express = require("express");
+const cors = require("cors");
+
+dotenv.config();
+
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.post("/keitaro-postback", async (req, res) => {
+    const data = req.body;
+    console.log("Полученные данные от Keitaro:", data);
+
+    const message = `Полученные данные от Keitaro:\n${JSON.stringify(data, null, 2)}`;
+
+    try {
+        await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
+            chat_id: process.env.TELEGRAM_CHAT_ID,
+            text: message,
+        });
+
+        res.send({ success: true, message: "Postback обработан и отправлен в Telegram" });
+    } catch (error) {
+        console.error("Ошибка при отправке в Telegram:", error);
+        res.status(500).send({ success: false, message: "Не удалось отправить данные в Telegram" });
+    }
+});
 
 app.get("/", (req, res) => res.send("Express ready on Vercel"));
 
