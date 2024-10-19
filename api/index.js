db1e7a152f56364bb9c7d535a961271d8a8a4178
vladyslavsaienko <vladyslav.saienko@onicore.io>
2024-10-20 00:32:45 +0300

    test2

--- api/index.js
+++ api/index.js
@@ -4,23 +4,20 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("Successfully connected to MongoDB");
-        app.listen(3000, () => console.log("Server ready on port 3000."));
-    })
-    .catch((error) => {
-        console.error("Failed to connect to MongoDB:", error);
-        process.exit(1);
-    });
-app.get("/", (req, res) => res.send("Express on Vercel =)"));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(error => console.error("Failed to connect to MongoDB:", error));
+
 app.get("/check-db", async (req, res) => {
     try {
         const result = await mongoose.connection.db.collection("postbacks").findOne();
         res.send(result ? "DB is reachable!" : "DB is empty!");
     } catch (error) {
-        res.status(500).send("Error accessing the database: " + error);
+        console.error("Error accessing the database:", error);
+        res.status(500).send("Error accessing the database: " + error.message);
     }
 });
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
